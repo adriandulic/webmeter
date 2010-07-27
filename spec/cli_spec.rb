6af64786f745e911c914177aa78f555e4c13bc73
Adrian Dulić <adulic@gmail.com>
2010-07-28 00:41:22 +0200

    spec context for cli

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,8 +1,10 @@
 require 'webmeter/cli'
 
-describe Webmeter do
-  it "should parse options" do
-    cli = Webmeter::CLI.execute(["--all"])
-    cli.options.should == ["--all"]
+describe "Webmeter" do
+  context "CLI" do
+    it "should parse options" do
+      cli = Webmeter::CLI.execute(["--all"])
+      cli.options.should == ["--all"]
+    end
   end
 end
