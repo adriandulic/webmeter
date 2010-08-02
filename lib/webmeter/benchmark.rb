e43acae12473cfa5f6dbf4bc428382a1a2f1d62b
adriandulic <adulic@gmail.com>
2010-08-02 13:18:58 +0200

    fixed threads

--- lib/webmeter/benchmark.rb
+++ lib/webmeter/benchmark.rb
@@ -3,33 +3,26 @@ module Webmeter
     def self.bench(host = "www.example.com")
       
       parser = Parser.new("access.log")
-      paths = parser.parse[0,20] # first 20 paths to request
+      paths = parser.parse # first 20 paths to request
       workers = 10 # number of users
       
-      statistics = []
-      responses = []
+      threads = []
       workers.times { |w|
         worker = Worker.new(host, paths)
         worker.user_agent(w)
-        statistics << worker.run
-        responses << worker.responses
+        threads << worker.run
       }
+      
+      start = Time.now
+      threads.each { |thread| thread.join }
+      time = (Time.now - start).to_f
+      requests = paths.size * workers
+      rps = requests / time;
 
-      success = redirect = client_error = server_error = 0
-
-      responses.each { |r|
-        success += r[:success]
-        redirect += r[:redirect]
-        client_error += r[:client_error]
-        server_error += r[:server_error]
-      }
-
-      puts "Webmeter : #{host}"
-      puts "Req/sec #{statistics.mean}"
-      puts "2XX #{success}"
-      puts "3XX #{redirect}"
-      puts "4XX #{client_error}"
-      puts "4XX #{server_error}"
+      puts "Webmeter #{host}"
+      puts "Time taken for test #{time}"
+      puts "Requests #{requests}"
+      puts "Req/sec #{rps}"
     end
   end
 end
